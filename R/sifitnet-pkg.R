d5385689926224853805d766aba3b9bb20f65884
owzar001 <owzar001@duke.edu>
2024-07-09 12:15:42 -0400

    merging in CRAN compatible version

---
+++ R/sifitnet-pkg.R
@@ -0,0 +1,17 @@
+#' @description
+#' To learn more about SiFINeT, start with the vignettes:
+#' `browseVignettes(package = "SiFINeT")`
+#' @useDynLib SiFINeT, .registration = TRUE
+#' @name sifinet-pkg
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats pnorm
+#' @importFrom stats quantile
+#' @importFrom methods as
+#' @importFrom methods new
+#' @importFrom utils combn
+#' @import Matrix
+#' @import quantreg
+#' @import igraph
+#' @import ggraph
+#' @import ggplot2
+"_PACKAGE" 
